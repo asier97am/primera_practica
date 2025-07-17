bf35408eff2c906349495b6c5c7e57558c99c524
asier97am <asier.aranda.malagon@gmail.com>
2025-07-17 19:59:39 +0200

    subiendo cosas

---
+++ conceptos programacion funcional/Leccio╠ün 1 - Conceptos de la Programacio╠ün funcional/src/funciones.py
@@ -0,0 +1,28 @@
+def sumar(a, b):
+    """
+    Funcion para sumar en python src
+    """
+    return a+b
+
+
+def restar(a, b):
+    """
+    Funcion para restar en python src
+    """
+    return a-b
+
+
+def multiplicar(a, b):
+    """
+    Funcion para multiplicar en python src
+    """
+    return a*b
+
+
+def dividir(a, b):
+    """
+    Funcion para dividir en python src
+    """
+    return a//b
+
+
